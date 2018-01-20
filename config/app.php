3dff39dab9d09e8f41f8b11597292a6115049ac7
Msangi <alaminhamadi@gmail.com>
2018-01-20 19:08:50 +0300

    database config

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:k98I+5R07914RtWTOzzYihUK3azWA3Dk6ADzBNYFma8='),
 
     'cipher' => 'AES-256-CBC',
 
