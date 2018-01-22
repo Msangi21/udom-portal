4121c3126a7fa0445eaca695c8a3e66ea41b3b0d
Msangi <alaminhamadi@gmail.com>
2018-01-22 18:29:59 +0300

    login problem 1

--- resources/views/dashboard/seller/partials/side_bar.blade.php
+++ resources/views/dashboard/seller/partials/side_bar.blade.php
@@ -56,7 +56,7 @@
                                 <tr>
                                     <td>
                                        
-                                        <span class="glyphicon glyphicon-stats text-success"></span><a href="/address/{{Auth::id()}}/edit"><span style="padding: 12px">Status {{ Auth::User()->status }}
+                                        <span class="glyphicon glyphicon-stats text-success"></span><a href="/address/{{Auth::id()}}/edit"><span style="padding: 12px">Status 
                                         </span></a> 
 
                                         
