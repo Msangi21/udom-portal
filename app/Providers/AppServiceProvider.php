eb3d051c2c1f4b62c57082c71c033e9c8c18d987
Msangi <alaminhamadi@gmail.com>
2018-01-23 15:10:22 +0300

    login problem 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(UrlGenerator $url)
     {
-        if(env('APP_ENV') !== 'local')
+        if(env('APP_ENV') != 'local')
         {
             $url->forceSchema('https');
         }
